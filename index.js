1c8ffd9e3a3246b5877e89c68755dbe8d23646ac
waxrofalkin <168885245+waxrofalkin@users.noreply.github.com>
2024-05-06 05:15:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ user.on('loggedOn', () => {
 });
 
 
-// var username2 = process.env.username2;
-// var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
+ var username2 = process.env.username2;
+ var password2 = process.env.password2;
+ var shared_secret2 = process.env.shared2;
 
-// var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
-// var status2 = 1;  // 1 - online, 7 - invisible
+ var games2 = [730];  // Enter here AppIDs of the needed games
+ var status2 = 1;  // 1 - online, 7 - invisible
 
 
-// user2 = new steamUser();
-// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
-// user2.on('loggedOn', () => {
-// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
-// 	user2.gamesPlayed(games2);
-// });
+ user2 = new steamUser();
+ user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
+ user2.on('loggedOn', () => {
+ 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
+ 	user2.setPersona(status2);               
+ 	user2.gamesPlayed(games2);
+ });
